69608933432f39f8f9fd82c4045fb7af99bf0602
Максим <yakhont04@mail.ru>
2026-04-04 03:38:10 +0300

    added choosing color

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let isDrawing = false
 
+const color = document.querySelector('#myColor')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -7,31 +8,38 @@ let x = 0
 let y = 0
 let width = 10
 let height = 10
+let currentColor = color.value
 
 canvas.addEventListener('mousedown', handleClick)
 canvas.addEventListener('mousemove', handleDraw)
 canvas.addEventListener('mouseup', endDraw)
 canvas.addEventListener('mouseleave', endDraw)
+color.addEventListener('change', changeCurrentColor)
 
 function resizeCanvas() {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
+  canvas.width = 500
+  canvas.height = 500
 }
 
 resizeCanvas()
 
-window.addEventListener('resize', resizeCanvas)
+function changeCurrentColor(event) {
+  currentColor = event.target.value
+  console.log(currentColor)
+  console.log(event.target.value)
+}
 
 function handleClick(event) {
   if (event.button === 0) {
     isDrawing = true
     x = event.offsetX
     y = event.offsetY
+    ctx.fillStyle = currentColor
     ctx.fillRect(
       x - Math.floor(width / 2),
       y - Math.floor(height / 2),
       width,
-      height
+      height,
     )
     console.log(x, y)
   }
@@ -41,11 +49,12 @@ function handleDraw(event) {
   if (isDrawing) {
     x = event.offsetX
     y = event.offsetY
+    ctx.fillStyle = currentColor
     ctx.fillRect(
       x - Math.floor(width / 2),
       y - Math.floor(height / 2),
       width,
-      height
+      height,
     )
     console.log(x, y)
   }
@@ -55,4 +64,4 @@ function endDraw() {
   isDrawing = false
 }
 
-console.log(canvas)
+// console.log(canvas)
