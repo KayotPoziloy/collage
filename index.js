67fde2a0476113093d876afd7a6b62ec55833512
Максим <yakhont04@mail.ru>
2026-05-01 15:43:54 +0300

    added drawing with an image (without selecting an image)

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 let isDrawing = false
 
-const color = document.querySelector('#myColor')
+const color = document.querySelector('#brushColor')
+const imageCheckbox = document.querySelector('#imageBrush')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
+const image = new Image()
+
+image.src =
+  'https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Daria_Morgendorffer.png/250px-Daria_Morgendorffer.png'
+
+console.log(image)
+console.log(image.height, image.width)
 
 let x = 0
 let y = 0
 let width = 10
 let height = 10
+let imageWidth = image.width
+let imageHeight = image.height
 let currentColor = color.value
+let isImageDraw = false
 
 canvas.addEventListener('mousedown', handleClick)
 canvas.addEventListener('mousemove', handleDraw)
+
 canvas.addEventListener('mouseup', endDraw)
 canvas.addEventListener('mouseleave', endDraw)
 color.addEventListener('change', changeCurrentColor)
+imageCheckbox.addEventListener('click', () => {
+  isImageDraw = imageCheckbox.checked
+  console.log(isImageDraw)
+})
 
 function resizeCanvas() {
   canvas.width = 500
@@ -32,30 +48,45 @@ function changeCurrentColor(event) {
 function handleClick(event) {
   if (event.button === 0) {
     isDrawing = true
-    x = event.offsetX
-    y = event.offsetY
-    ctx.fillStyle = currentColor
-    ctx.fillRect(
-      x - Math.floor(width / 2),
-      y - Math.floor(height / 2),
-      width,
-      height,
-    )
+
+    if (isImageDraw) {
+      x = event.offsetX - Math.floor(imageWidth / 2)
+      y = event.offsetY - Math.floor(imageHeight / 2)
+      ctx.drawImage(image, x, y)
+    } else {
+      x = event.offsetX
+      y = event.offsetY
+      ctx.fillStyle = currentColor
+      ctx.fillRect(
+        x - Math.floor(width / 2),
+        y - Math.floor(height / 2),
+        width,
+        height,
+      )
+    }
+
     console.log(x, y)
   }
 }
 
 function handleDraw(event) {
   if (isDrawing) {
-    x = event.offsetX
-    y = event.offsetY
-    ctx.fillStyle = currentColor
-    ctx.fillRect(
-      x - Math.floor(width / 2),
-      y - Math.floor(height / 2),
-      width,
-      height,
-    )
+    if (isImageDraw) {
+      x = event.offsetX - Math.floor(imageWidth / 2)
+      y = event.offsetY - Math.floor(imageHeight / 2)
+      ctx.drawImage(image, x, y)
+    } else {
+      x = event.offsetX
+      y = event.offsetY
+      ctx.fillStyle = currentColor
+      ctx.fillRect(
+        x - Math.floor(width / 2),
+        y - Math.floor(height / 2),
+        width,
+        height,
+      )
+    }
+
     console.log(x, y)
   }
 }
